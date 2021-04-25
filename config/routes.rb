785634694b00a055927899ebf81da9cc2c97cc8b
Paselan91 <kg03272016@gmail.com>
2021-04-25 09:50:34 +0900

    envファイル整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users/index'
+
+  resources :ticker, only: %i[index new]
 end
