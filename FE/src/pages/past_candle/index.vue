36f377139e37f692a87034379a83a6a7abac926f
Paselan91 <kg03272016@gmail.com>
2021-05-10 09:20:17 +0900

    クエリパラメータ(period,date)を選択するUIを作成

--- FE/src/pages/past_candle/index.vue
+++ FE/src/pages/past_candle/index.vue
@@ -5,14 +5,59 @@
 <template>
   <div>
     <h1>Past Candle</h1>
-    <!-- <div>
-      <p>choose period</p>
+    <div>
+      <h3>Choose period</h3>
       <v-select
+        v-model="select"
         :items="period"
         label="Period"
+        item-text="label"
+        item-value="value"
         solo
+        return-object
       ></v-select>
-    </div> -->
+    </div>
+    <div>
+      <h3>Choose date</h3>
+      <v-container fluid>
+        <p>{{ isBefore }}</p>
+        <v-radio-group v-model="isBefore" mandatory>
+          <v-radio label="Before" value="true"></v-radio>
+          <v-radio label="After" value="false"></v-radio>
+        </v-radio-group>
+      </v-container>
+
+      <v-container>
+        <v-row>
+          <v-col cols="12" lg="6">
+            <v-menu
+              :close-on-content-click="false"
+              transition="scale-transition"
+              offset-y
+              max-width="290px"
+              min-width="auto"
+            >
+              <template #activator="{ on, attrs }">
+                <v-text-field
+                  v-model="computedDateFormatted"
+                  label="Date"
+                  hint="MM/DD/YYYY format"
+                  persistent-hint
+                  prepend-icon="mdi-calendar"
+                  v-bind="attrs"
+                  @blur="date = parseDate(dateFormatted)"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="date" no-title></v-date-picker>
+            </v-menu>
+            <p>
+              Date in ISO format: <strong>{{ date }}</strong>
+            </p>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
     <div>
       <v-btn @click="fetchPastCandle">Get Data from API</v-btn>
     </div>
@@ -34,7 +79,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "nuxt-property-decorator"
+import { Component, Prop, Vue, Watch } from "nuxt-property-decorator"
 import axios from "axios"
 
 @Component({})
@@ -42,6 +87,53 @@ export default class GenericChart extends Vue {
   isLoading: Boolean = false
   duration: string = "minutes" // TODO: hour, minutes, seconds 可変にする
   url: string = "/api/v1/fetch_past_ticker/"
+
+  // periodに関する処理
+  period: Array<object> = [
+    { label: "1m", value: 60 },
+    { label: "3m", value: 180 },
+    { label: "5m", value: 300 },
+    { label: "5m", value: 900 },
+    { label: "15m", value: 1800 },
+    { label: "30m", value: 3600 },
+    { label: "1h", value: 7200 },
+    { label: "2h", value: 14400 },
+    { label: "4h", value: 21600 },
+    { label: "6h", value: 43200 },
+    { label: "12h", value: 86400 },
+    { label: "1d", value: 259200 },
+    { label: "3d", value: 604800 }
+  ]
+
+  select = { label: "1m", value: 60 }
+
+  isBefore: Boolean = true
+
+  date = new Date().toISOString().substr(0, 10)
+  dateFormatted = this.formatDate(new Date().toISOString().substr(0, 10))
+  get computedDateFormatted() {
+    return this.formatDate(this.date)
+  }
+
+  formatDate(date) {
+    if (!date) return null
+
+    const [year, month, day] = date.split("-")
+    return `${month}/${day}/${year}`
+  }
+
+  parseDate(date) {
+    if (!date) return null
+
+    const [month, day, year] = date.split("/")
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`
+  }
+
+  @Watch("data")
+  watchDate() {
+    this.dateFormatted = this.formatDate(this.date)
+  }
+
   public series = [
     {
       data: []
