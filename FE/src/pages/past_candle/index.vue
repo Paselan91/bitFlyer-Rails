c23052750a2a38b21d961e00e77b48e5a02520a6
Paselan91 <kg03272016@gmail.com>
2021-05-17 00:32:25 +0900

    デザインを微修正

--- FE/src/pages/past_candle/index.vue
+++ FE/src/pages/past_candle/index.vue
@@ -4,80 +4,109 @@
 </template> -->
 <template>
   <div>
-    <h1>Past Candle</h1>
-    <div>
-      <h3>Choose period</h3>
-      <v-select
-        v-model="selectedPeriod"
-        :items="period"
-        label="Period"
-        item-text="label"
-        item-value="value"
-        solo
-        return-object
-      ></v-select>
-    </div>
-    <div>
-      <h3>Choose date</h3>
-      <v-container fluid>
-        <v-radio-group v-model="beforeAfter" mandatory>
-          <v-radio label="After" value="after"></v-radio>
-          <v-radio label="Before" value="before"></v-radio>
-        </v-radio-group>
-      </v-container>
+    <v-container class="grey lighten-5 text-center">
+      <h1>Past Candle</h1>
+      <v-row :align="align" no-gutters>
+        <v-col :cols="5">
+          <v-container>
+            <h3 class="text-center">Choose Period</h3>
+            <v-select
+              v-model="selectedPeriod"
+              :items="period"
+              label="Period"
+              item-text="label"
+              item-value="value"
+              solo
+              return-object
+              width="100px"
+            ></v-select>
+          </v-container>
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col :cols="7">
+          <h3 class="text-center">Choose Date</h3>
+          <v-row>
+            <v-col cols="5">
+              <v-container fluid>
+                <v-radio-group v-model="beforeAfter" mandatory col>
+                  <v-radio label="After" value="after"></v-radio>
+                  <v-radio label="Before" value="before"></v-radio>
+                </v-radio-group>
+              </v-container>
+            </v-col>
+            <v-col cols="7">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" lg="6">
+                    <v-menu
+                      :close-on-content-click="false"
+                      transition="scale-transition"
+                      offset-y
+                      max-width="290px"
+                      min-width="auto"
+                    >
+                      <template #activator="{ on, attrs }">
+                        <v-text-field
+                          v-model="computedDateFormatted"
+                          label="Date"
+                          persistent-hint
+                          prepend-icon="mdi-calendar"
+                          v-bind="attrs"
+                          @blur="date = parseDate(dateFormatted)"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="date" no-title></v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3" offset="6">
+          <h3>You selected</h3>
+          <p>
+            {{ this.selectedPeriod.label }} {{ this.beforeAfter }}
+            {{ this.date }}
+          </p>
+        </v-col>
+        <v-col cols="2">
+          <v-btn class="my-3" @click="fetchPastCandle">Get Data</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>
+      <client-only>
+        <div v-if="isLoading" class="text-center">
+          <v-progress-circular :size="50" indeterminate></v-progress-circular>
+        </div>
+        <div v-else id="chart">
+          <apexchart
+            type="candlestick"
+            height="350"
+            :options="chartOptions"
+            :series="series"
+          ></apexchart>
+        </div>
+      </client-only>
+    </v-container>
 
-      <v-container>
-        <v-row>
-          <v-col cols="12" lg="6">
-            <v-menu
-              :close-on-content-click="false"
-              transition="scale-transition"
-              offset-y
-              max-width="290px"
-              min-width="auto"
-            >
-              <template #activator="{ on, attrs }">
-                <v-text-field
-                  v-model="computedDateFormatted"
-                  label="Date"
-                  hint="MM/DD/YYYY format"
-                  persistent-hint
-                  prepend-icon="mdi-calendar"
-                  v-bind="attrs"
-                  @blur="date = parseDate(dateFormatted)"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker v-model="date" no-title></v-date-picker>
-            </v-menu>
-            <p>
-              Date in ISO format: <strong>{{ date }}</strong>
-            </p>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-    <div>
-      <v-btn class="my-3" @click="fetchPastCandle">Get Data from API</v-btn>
-    </div>
-    <v-btn class="my-3" depressed>
-      <nuxt-link to="/">back</nuxt-link>
-    </v-btn>
+    <v-container class="my-5">
+      <v-row justify="center">
+        <v-btn to="/" nuxt elevation="3" width="10%">
+          Back
+        </v-btn>
+      </v-row>
+    </v-container>
 
-    <v-btn class="my-3" depressed color="secondary" @click="checkParamerters">
-      check query paramerters
-    </v-btn>
-
-    <client-only>
-      <div v-if="!isLoading" id="chart">
-        <apexchart
-          type="candlestick"
-          height="350"
-          :options="chartOptions"
-          :series="series"
-        ></apexchart>
-      </div>
-    </client-only>
+    <v-container>
+      <v-btn class="my-3 text-center" @click="checkParamerters"
+        >Check Qeury Parameters</v-btn
+      >
+    </v-container>
   </div>
 </template>
 
@@ -96,27 +125,28 @@ export default class GenericChart extends Vue {
     { label: "1m", value: 60 },
     { label: "3m", value: 180 },
     { label: "5m", value: 300 },
-    { label: "5m", value: 900 },
-    { label: "15m", value: 1800 },
-    { label: "30m", value: 3600 },
-    { label: "1h", value: 7200 },
-    { label: "2h", value: 14400 },
-    { label: "4h", value: 21600 },
-    { label: "6h", value: 43200 },
-    { label: "12h", value: 86400 },
-    { label: "1d", value: 259200 },
-    { label: "3d", value: 604800 }
+    { label: "15m", value: 900 },
+    { label: "30m", value: 1800 },
+    { label: "1h", value: 3600 },
+    { label: "2h", value: 7200 },
+    { label: "4h", value: 14400 },
+    { label: "6h", value: 21600 },
+    { label: "12h", value: 43200 },
+    { label: "1d", value: 86400 },
+    { label: "3d", value: 259200 },
+    { label: "1w", value: 604800 }
   ]
 
-  selectedPeriod = { label: "1m", value: 60 }
+  selectedPeriod = { label: "15m", value: 900 }
 
+  today = new Date()
   date = new Date().toISOString().substr(0, 10)
   dateFormatted = this.formatDate(new Date().toISOString().substr(0, 10))
   get computedDateFormatted() {
     return this.formatDate(this.date)
   }
 
-  // TODO: デバッグよう
+  // TODO: デバッグ用
   checkParamerters() {
     console.log("checkParamerters")
     console.log("selected period ")
@@ -180,6 +210,13 @@ export default class GenericChart extends Vue {
       min: undefined,
       max: undefined
     }
+  }
+
+  mounted() {
+    const yesterday = new Date(this.today)
+    yesterday.setDate(yesterday.getDate() - 1)
+    this.date = yesterday.toISOString().substr(0, 10)
+    this.fetchPastCandle()
   }
 
   async fetchPastCandle() {
